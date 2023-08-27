696532e310f8c918185e8574ae6336d2727fcf14
krtuhin <tuhin.km2774@gmail.com>
2023-08-27 21:59:35 +0530

    Project Created

---
+++ src/main/java/com/exceltodbjson/ExcelToDbJsonApplication.java
@@ -0,0 +1,13 @@
+package com.exceltodbjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelToDbJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelToDbJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exceltodbjson/ExcelToDbJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exceltodbjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelToDbJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
